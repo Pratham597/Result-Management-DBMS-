232e2005d97727ad7a7ca8fcae192fff7277db2a
Pratham7195 <pratham7195@gmail.com>
2024-11-29 16:56:59 +0530

    Creating pool of db

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -1,20 +1,26 @@
 import mysql from 'mysql2/promise';
 
-const connectDB=async ()=>{
-    let connection;
-    try {
-        const access = {
-            host:`${process.env.DB_HOST}`,
-            user: `${process.env.DB_USER}`,
-            database: `${process.env.DB_NAME}`,
-            password: `${process.env.DB_PASSWORD}`
-        };
-        connection=await mysql.createConnection(access);
-        console.log('Connected to the database successfully :)');
-    } catch (error) {
-        console.log('Failed to connect!');
+let pool;
+
+const connectDB = async () => {
+    if (!pool) {
+        try {
+            pool = mysql.createPool({
+                host: process.env.DB_HOST,
+                user: process.env.DB_USER,
+                database: process.env.DB_NAME,
+                password: process.env.DB_PASSWORD,
+                waitForConnections: true,
+                connectionLimit: 10, // Adjust as needed
+                queueLimit: 0,
+            });
+            console.log('Connected to the database successfully :)');
+        } catch (error) {
+            console.error('Failed to connect to the database:', error.message);
+            throw error; // Let the app know the connection failed
+        }
     }
-    return connection;
-}
+    return pool;
+};
 
 export default connectDB;
