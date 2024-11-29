be8e626e40a6300633ec7c7274f9540581313fea
Pratham7195 <pratham7195@gmail.com>
2024-11-29 16:27:07 +0530

    Client Side Routing

--- backend/server.js
+++ backend/server.js
@@ -243,6 +243,7 @@ app.all('*',(req,res,next)=>{
 })
 
 app.use((err, req, res) => {
+  console.log(err.message)
   return res.status(500).json({ message: "Internal Server Error!" });
 });
 app.listen(3000, () => {
